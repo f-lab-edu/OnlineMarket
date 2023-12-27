ebcc8c8e82fb65ac48f3618ea80a55dc93994be5
minjung <mingmong0018@gmail.com>
2023-12-28 04:45:29 +0900

    fix: userRepository가 추가된 후 구조가 변경되어 테스트 코드 수정

--- src/main/java/com/market/service/UserService.java
+++ src/main/java/com/market/service/UserService.java
@@ -1,6 +1,5 @@
 package com.market.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.market.domain.User;
@@ -8,10 +7,12 @@ import com.market.repository.UserRepository;
 
 @Service
 public class UserService {
-	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userRepository = new UserRepository();
 
 	public void signUp(User user) {
 		user.createdUser();
+		if (!userRepository.insertUser(user)) {
+			throw new RuntimeException("회원가입 실패!!");
+		}
 	}
 }
--- src/test/java/com/market/UserServiceTest.java
+++ src/test/java/com/market/UserServiceTest.java
@@ -1,11 +1,14 @@
 package com.market;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.market.domain.User;
+import com.market.repository.UserRepository;
 import com.market.service.UserService;
 
 @SpringBootTest
@@ -14,15 +17,18 @@ public class UserServiceTest {
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	UserRepository userRepository;
+
 	@Test
 	@DisplayName("고객 회원가입 성공")
-	public void createUserTest() {
+	public void signUpTest() {
 		User user = new User(
 			"minjung123",
 			"asdfasdf12",
 			"김민정",
 			"010-1234-1234");
 		userService.signUp(user);
-		assertEquals("minjung123", userService.user.get("minjung123").getId());
+		assertTrue(userRepository.insertUser(user));
 	}
 }
