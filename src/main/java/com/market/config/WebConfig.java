cf7ad3614a3b4632bb6a35d7ee51eacd2a216a6d
minjung <mingmong0018@gmail.com>
2024-01-24 02:55:36 +0900

    refactor: WebConfig 의존성 주입을 생성자 주입으로 변경

--- src/main/java/com/market/config/WebConfig.java
+++ src/main/java/com/market/config/WebConfig.java
@@ -2,16 +2,17 @@ package com.market.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-	@Autowired
-	HandlerInterceptor authInterceptor;
+	private final HandlerInterceptor authInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
